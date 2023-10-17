59b99cf97fb0adcbc87c923fc1340652ffc57c33
tangldev <tech@tangl.dev>
2023-10-17 22:10:30 +0800

    progress

--- d9-burn-contracts/d9-main/lib.rs
+++ d9-burn-contracts/d9-main/lib.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 use d9_burn_common::{ BurnPortfolio, ActionRecord, D9Environment, Error };
 #[ink::contract(env = D9Environment)]
+// #[ink::contract(env = D9Environment)]
 mod d9_main {
     use super::*;
     use ink::storage::Mapping;
@@ -42,7 +43,7 @@ mod d9_main {
 
     impl D9Main {
         /// Constructor that initializes the `bool` value to the given `init_value`.
-        #[ink(constructor)]
+        #[ink(constructor, payable)]
         pub fn new(admin: AccountId, burn_contracts: Vec<AccountId>) -> Self {
             Self {
                 admin,
@@ -52,109 +53,95 @@ mod d9_main {
             }
         }
 
-        /// Burns a specified amount from the caller's account and logs the transaction.
-        ///
-        /// This function allows an account to burn an amount, which is then recorded
-        /// in their associated `BurnPortfolio`. The amount is deducted from the sender's
-        /// balance and transferred to this main account.
+        /// Executes a burn by making a cross-contract call, updates the total burned amount,
+        /// updates the user's portfolio, and emits a `BurnExecuted` event.
         ///
         /// # Arguments
         ///
-        /// * `burn_contract`: The account ID of the contract to which the burned amount will be sent.
+        /// * `burn_contract` - Account ID of the contract to call for the burn.
         ///
-        /// # Requirements
+        /// # Errors
         ///
-        /// * The caller must transfer a non-zero amount to this function.
-        /// * The specified `burn_contract` must be one of the valid burn contracts recognized by this contract.
+        /// Returns `Err` if the burn amount is zero, the burn contract is not valid,
+        /// or the cross-contract call fails.
         ///
         /// # Returns
         ///
-        /// * On success: Returns the updated `BurnPortfolio` for the caller.
-        /// * On error: Returns an `Error` indicating the reason for the failure, such as insufficient burn amount or invalid burn contract.
-        ///
-        /// # Panics
-        ///
-        /// This function does not explicitly panic but relies on the behavior of the called burn contract.
-        /// If the called burn contract reverts or fails, this function will propagate the error.
-        ///
-        /// # Notes
-        ///
-        /// * The function uses `ink::selector_bytes` to determine the function signature for the `burn_contract`.
-        /// * Updates to the `BurnPortfolio` are persisted to storage.
-        ///
+        /// Returns `Ok` with the updated portfolio on success.
         #[ink(message, payable)]
         pub fn burn(&mut self, burn_contract: AccountId) -> Result<BurnPortfolio, Error> {
-            let burn_amount: Balance = self.env().transferred_value();
-            let account_id: AccountId = self.env().caller();
-            // burn amount must at least be greater than zero otherwise
-            // we permit the called contract to determin its own minimum burn amount
+            let caller = self.env().caller();
+            let burn_amount = self.env().transferred_value();
+
+            // Ensure the burn amount is sufficient
             if burn_amount == 0 {
                 return Err(Error::BurnAmountInsufficient);
             }
+
+            // Verify the burn contract
             if !self.burn_contracts.contains(&burn_contract) {
                 return Err(Error::InvalidBurnContract);
             }
 
-            // cross contract call
-            let burn_result = build_call::<D9Environment>()
-                .call(burn_contract)
-                .gas_limit(0)
-                .exec_input(
-                    ExecutionInput::new(Selector::new(ink::selector_bytes!("portfolio_execute")))
-                        .push_arg(account_id)
-                        .push_arg(burn_amount)
-                )
-                .returns::<Result<Balance, Error>>()
-                .invoke();
+            // Make the cross-contract call
+            let balance_increase = match
+                build_call::<D9Environment>()
+                    .call(burn_contract)
+                    .gas_limit(0) // replace with an appropriate gas limit
+                    .exec_input(
+                        ExecutionInput::new(
+                            Selector::new(ink::selector_bytes!("controller_restricted_burn"))
+                        )
+                            .push_arg(caller)
+                            .push_arg(burn_amount)
+                    )
+                    .returns::<Result<Balance, Error>>()
+                    .invoke()
+            {
+                Ok(balance) => balance,
+                Err(e) => {
+                    return Err(e);
+                } // Handle potential call error
+            };
 
+            // Update portfolio and total burn
             let last_burn = ActionRecord {
                 time: self.env().block_timestamp(),
                 contract: burn_contract,
             };
             self.total_amount_burned = self.total_amount_burned.saturating_add(burn_amount);
+
+            let mut portfolio = self.portfolios.get(caller).unwrap_or(BurnPortfolio {
+                amount_burned: 0,
+                balance_due: 0,
+                balance_paid: 0,
+                last_withdrawal: None,
+                last_burn: last_burn.clone(), // clone required for new portfolios
+            });
+            portfolio.amount_burned = portfolio.amount_burned.saturating_add(burn_amount);
+            portfolio.balance_due = portfolio.balance_due.saturating_add(balance_increase);
+            portfolio.last_burn = last_burn;
+
+            // Emit an event for the burn execution
             self.env().emit_event(BurnExecuted {
-                from: self.env().caller(),
+                from: caller,
                 amount: burn_amount,
             });
-            //process cross contract call result
-            match burn_result {
-                Ok(balance_increment) => {
-                    if let Some(mut portfolio) = self.portfolios.get(&account_id) {
-                        portfolio.amount_burned =
-                            portfolio.amount_burned.saturating_add(burn_amount);
-                        portfolio.balance_due =
-                            portfolio.balance_due.saturating_add(balance_increment);
-                        portfolio.last_burn = last_burn;
-                        self.portfolios.insert(account_id, &portfolio);
-                        Ok(portfolio)
-                    } else {
-                        let portfolio = BurnPortfolio {
-                            amount_burned: burn_amount,
-                            balance_due: balance_increment,
-                            balance_paid: 0,
-                            last_withdrawal: None,
-                            last_burn: last_burn,
-                        };
-                        self.portfolios.insert(account_id, &portfolio);
-                        Ok(portfolio)
-                    }
-                }
-                Err(e) => Err(e),
-            }
+            self.portfolios.insert(caller, &portfolio);
+            Ok(portfolio.clone()) // clone for returning; original is in the map
         }
 
         #[ink(message)]
         pub fn withdraw(&mut self, burn_contract: AccountId) -> Result<BurnPortfolio, Error> {
-            if !self.burn_contracts.contains(&burn_contract) {
-                return Err(Error::InvalidBurnContract);
-            }
+            self.burn_contracts
+                .contains(&burn_contract)
+                .then(|| ())
+                .ok_or(Error::InvalidBurnContract)?;
+
             let account_id: AccountId = self.env().caller();
-            let maybe_portfolio = self.portfolios.get(&account_id);
-            if maybe_portfolio.is_none() {
-                return Err(Error::NoAccountFound);
-            }
-            let portfolio = maybe_portfolio.unwrap();
-            let allowance_result = build_call::<D9Environment>()
+            let mut portfolio = self.portfolios.get(&account_id).ok_or(Error::NoAccountFound)?;
+
+            let (withdraw_allowance, last_withdrawal_timestamp) = build_call::<D9Environment>()
                 .call(burn_contract)
                 .gas_limit(0)
                 .exec_input(
@@ -163,32 +150,20 @@ mod d9_main {
                     ).push_arg(account_id)
                 )
                 .returns::<Result<(Balance, Timestamp), Error>>()
-                .invoke();
-
-            match allowance_result {
-                Ok((withdrawal_allowance, last_withdrawal_timestamp)) => {
-                    self.env()
-                        .transfer(account_id, withdrawal_allowance)
-                        .expect("Transfer failed.");
-                    self.total_amount_burned =
-                        self.total_amount_burned.saturating_add(withdrawal_allowance);
-                    let updated_portfolio = BurnPortfolio {
-                        amount_burned: portfolio.amount_burned,
-                        balance_due: portfolio.balance_due.saturating_sub(withdrawal_allowance),
-                        balance_paid: portfolio.balance_paid.saturating_add(withdrawal_allowance),
-                        last_withdrawal: Some(ActionRecord {
-                            time: last_withdrawal_timestamp,
-                            contract: burn_contract,
-                        }),
-                        last_burn: portfolio.last_burn,
-                    };
-                    self.portfolios.insert(account_id, &updated_portfolio);
-                    Ok(updated_portfolio)
-                }
-                Err(e) => {
-                    return Err(e);
-                }
+                .invoke()?;
+            if withdraw_allowance == 0 {
+                return Ok(portfolio);
             }
+            portfolio.balance_due = portfolio.balance_due.saturating_sub(withdraw_allowance);
+            portfolio.balance_paid = portfolio.balance_paid.saturating_add(withdraw_allowance);
+            portfolio.last_withdrawal = Some(ActionRecord {
+                time: last_withdrawal_timestamp,
+                contract: burn_contract,
+            });
+
+            self.portfolios.insert(account_id, &portfolio);
+
+            Ok(portfolio.clone())
         }
 
         #[ink(message)]
@@ -204,6 +179,12 @@ mod d9_main {
             Ok(())
         }
 
+        #[ink(message)]
+        pub fn remove_burn_contract(&mut self, burn_contract: AccountId) {
+            assert!(self.burn_contracts.contains(&burn_contract), "BurnContract not found");
+            // assert!(self.env().caller() != self.admin, "Invalid caller");
+            self.burn_contracts.retain(|&x| x != burn_contract);
+        }
         #[ink(message)]
         pub fn get_admin(&self) -> AccountId {
             self.admin
@@ -227,21 +208,132 @@ mod d9_main {
     mod tests {
         /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
+    }
+    #[cfg(all(test, feature = "e2e-tests"))]
+    mod e2e_tests {
+        use super::*;
+        use ink_e2e::build_message;
+        use d9_burn_mining::d9_burn_mining::D9burnMining;
+        use d9_burn_mining::d9_burn_mining::D9burnMiningRef;
+        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let d9referral = D9BurnMining::default();
-            assert_eq!(d9referral.get(), false);
+        #[ink_e2e::test]
+        async fn burn_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+            //prepare main contract
+            let main_constructor = D9MainRef::new(
+                ink_e2e::account_id(ink_e2e::AccountKeyring::Alice),
+                vec![]
+            );
+            let main_contract_address = client
+                .instantiate("d9_main", &ink_e2e::alice(), main_constructor, 0, None).await
+                .expect("Failed to instantiate contract").account_id;
+
+            //prepare burn contract
+            let burn_constructor = D9burnMiningRef::new(main_contract_address, 100);
+            let burn_contract_address = client
+                .instantiate("d9_burn_mining", &ink_e2e::alice(), burn_constructor, 0, None).await
+                .expect("Failed to instantiate contract").account_id;
+
+            // add burn contract to main contract
+            let add_burn_contract_call = build_message::<D9MainRef>(
+                main_contract_address.clone()
+            ).call(|d9_main| d9_main.add_burn_contract(burn_contract_address.clone()));
+
+            let add_burn_contract_response = client.call(
+                &ink_e2e::alice(),
+                add_burn_contract_call,
+                0,
+                None
+            ).await;
+
+            assert!(add_burn_contract_response.is_ok());
+
+            let burn_call = build_message::<D9MainRef>(main_contract_address.clone()).call(|d9_main|
+                d9_main.burn(burn_contract_address.clone())
+            );
+            let burn_amount = 500;
+            let burn_response = client.call(
+                &ink_e2e::alice(),
+                burn_call,
+                burn_amount.clone(),
+                None
+            ).await;
+
+            assert!(burn_response.is_ok());
+
+            let get_burn_amount_call = build_message::<D9MainRef>(
+                main_contract_address.clone()
+            ).call(|d9_main| d9_main.get_total_burned());
+
+            let get_burn_amount_response = client.call(
+                &ink_e2e::alice(),
+                get_burn_amount_call,
+                0,
+                None
+            ).await;
+
+            assert!(get_burn_amount_response.is_ok());
+            let total_burned = get_burn_amount_response.unwrap().return_value();
+            assert_eq!(total_burned, burn_amount);
+            Ok(())
         }
 
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut d9referral = D9BurnMining::new(false);
-            assert_eq!(d9referral.get(), false);
-            d9referral.flip();
-            assert_eq!(d9referral.get(), true);
+        #[ink_e2e::test]
+        async fn withdraw_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+            //prepare main contract
+            let main_constructor = D9MainRef::new(
+                ink_e2e::account_id(ink_e2e::AccountKeyring::Alice),
+                vec![]
+            );
+            let main_contract_address = client
+                .instantiate(
+                    "d9_main",
+                    &ink_e2e::alice(),
+                    main_constructor,
+                    1000000000000,
+                    None
+                ).await
+                .expect("Failed to instantiate main contract").account_id;
+
+            //prepare burn contract
+            let burn_constructor = D9burnMiningRef::new(main_contract_address, 100);
+            let burn_contract_address = client
+                .instantiate("d9_burn_mining", &ink_e2e::alice(), burn_constructor, 0, None).await
+                .expect("Failed to instantiate burn contract").account_id;
+
+            // add burn contract to main contract
+            let add_burn_contract_call = build_message::<D9MainRef>(
+                main_contract_address.clone()
+            ).call(|d9_main| d9_main.add_burn_contract(burn_contract_address.clone()));
+
+            let add_burn_contract_response = client.call(
+                &ink_e2e::alice(),
+                add_burn_contract_call,
+                0,
+                None
+            ).await;
+
+            assert!(add_burn_contract_response.is_ok());
+
+            let burn_call = build_message::<D9MainRef>(main_contract_address.clone()).call(|d9_main|
+                d9_main.burn(burn_contract_address.clone())
+            );
+            let burn_amount = 500;
+            let burn_response = client.call(
+                &ink_e2e::alice(),
+                burn_call,
+                burn_amount.clone(),
+                None
+            ).await;
+
+            assert!(burn_response.is_ok());
+
+            let withdraw_call = build_message::<D9MainRef>(main_contract_address.clone()).call(
+                |d9_main| d9_main.withdraw(burn_contract_address.clone())
+            );
+            let withdraw_response = client.call(&ink_e2e::alice(), withdraw_call, 0, None).await;
+            assert!(withdraw_response.is_ok());
+            Ok(())
         }
     }
 }
